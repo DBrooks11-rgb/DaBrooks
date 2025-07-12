3a9eebd3b9f5bf0dbd06b74ecbc601d236a4a613
brooksda <brooksda@iu.edu>
2025-07-12 17:21:59 -0400

    More to basic card integration

--- js/modal.js
+++ js/modal.js
@@ -2,25 +2,56 @@
 
 // JS
 const beatles = {
-    john: {
-        name: 'John Lennon',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/8/85/John_Lennon_1969_%28cropped%29.jpg',
-        bio: `John Winston Ono Lennon MBE (born John Winston Lennon, 9 October 1940 – 8 December 1980) was an English singer, songwriter and peace activist who gained worldwide fame as the founder, co-lead vocalist, and rhythm guitarist of the Beatles. His songwriting partnership with Paul McCartney remains the most successful in history. In 1969, he started the Plastic Ono Band with his second wife, Yoko Ono. After the Beatles disbanded in 1970, Lennon continued as a solo artist and as Ono's collaborator.`,
+    cloud9: {
+        name: 'Cloud9',
+        image: "images/Cloud9_TitleFrame.png",
+        bio: `My 2025 Capstone Project: Cloud9 is a web platform that focuses
+                    on providing tools and resources to help those facing addiction to nicotine.
+                    I worked with Camilla Clark and Kevin Trancong to create a fully functional
+                    site that works with an SQL database and integrated Google API systems.
+                    I helped with back-end design, as well as extra input for front-end
+                    design aspects after collecting input from real experts in medical and
+                    UX design positions. This project required much time and effort to make
+                    it a reality, but I&rsquo;m very proud of how it came out in the end.`,
     },
-    paul: {
-        name: 'Paul McCartney',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/0/00/Paul_McCartney_Headshot_%28cropped%29.jpg',
-        bio: `Sir James Paul McCartney CH MBE (born 18 June 1942) is an English singer, songwriter, musician, composer, and record and film producer who gained worldwide fame as co-lead vocalist and bassist for the Beatles. His songwriting partnership with John Lennon remains the most successful in history. After the group disbanded in 1970, he pursued a solo career and formed the band Wings with his first wife, Linda, and Denny Laine.`,
+    airport: {
+        name: 'U.S. Airport Efficiency',
+        image: "images/US_Airport_Tableau_Cover.png",
+        bio: `I find myself wanting to travel more nowadays, and planes are 
+                    one of the biggest ways to travel today. But of course, airports aren’t 
+                    cheap, and many times, there are lots of costs that go into airports that 
+                    many people overlook. This tableau project aims to put my data analyst 
+                    skills to the test and display visualizations that tell the audience 
+                    useful information and an impactful story.`,
     },
-    george: {
-        name: 'George Harrison',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/George_Harrison_1974.jpg/1200px-George_Harrison_1974.jpg',
-        bio: `George Harrison MBE (25 February 1943 – 29 November 2001) was an English musician, singer-songwriter, and music and film producer who achieved international fame as the lead guitarist of the Beatles. Sometimes called "the quiet Beatle", Harrison embraced Indian culture and helped broaden the scope of popular music through his incorporation of Indian instrumentation and Hindu-aligned spirituality in the Beatles' work. Although the majority of the band's songs were written by John Lennon and Paul McCartney, most Beatles albums from 1965 onwards contained at least two Harrison compositions. His songs for the group included "Taxman", "Within You Without You", "While My Guitar Gently Weeps", "Here Comes the Sun" and "Something".`,
+    sleepwell: {
+        name: 'SleepWell',
+        image: "images/Mobile_screenshot.png",
+        bio: `One of my older projects. I worked on this mobile app prototype with Mukund Bhat, using Figma.
+                    I designed a 3D printed neckband that would serve to track the user&rsquo;s
+                    sleeping data in a final product. With my initial structure
+                    design aspects for the app and Mukund&rsquo;s polish, we created a strong
+                    demonstration of an interface that can be intergraded into a real-life
+                    system.`,
     },
-    ringo: {
-        name: 'Ringo Starr',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/3/32/Ringo_1963_%28cropped%29.jpg',
-        bio: `Sir Richard Starkey[2] MBE[3] (born 7 July 1940), known professionally as Ringo Starr, is an English musician, singer, songwriter and actor who gained worldwide fame as the drummer for the Beatles. He occasionally sang lead vocals with the group, usually for one song on each album, including "Yellow Submarine", "With a Little Help from My Friends" and their cover of "Act Naturally". He also wrote and sang the Beatles' songs "Don't Pass Me By" and "Octopus's Garden", and is credited as a co-writer of others. `,
+    inventory: {
+        name: 'Integrating Inventory',
+        image: "images/Inventory_Background.png",
+        bio: `This project had me working with members of the Army. 
+                    The problem involved improving the process of counting ammo and resources 
+                    in their inventory, with a multitude of parameters. This project had me 
+                    and my team communicating with many army members, all of which have 
+                    different perspectives and needs for our prototype program to be a success.`,
+    },
+    arduino: {
+        name: 'Arduino Christmas Tree',
+        image: "images/Christmas_NightLight.jpg",
+        bio: `After seeing so many Arduino projects, I went into trying 
+                    it myself, and I had a very fun experience. Niki Movva and I made a fun 
+                    light-up Christmas Tree design. I was in charge of designing the tree 
+                    using Fusion 360, while Niki was in charge of the circuits and the 
+                    lights. We both had a hand at the code, and we finished with a simple, 
+                    yet compelling masterpiece.`,
     },
 };
 
